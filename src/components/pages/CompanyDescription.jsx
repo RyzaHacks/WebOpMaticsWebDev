bd7246fff510431c937b539ac5a98c056d2316f0
ryzahacks <rack2cloud@gmail.com>
2023-12-24 20:36:42 +1100

    Your commit message here

--- src/components/pages/CompanyDescription.jsx
+++ src/components/pages/CompanyDescription.jsx
@@ -1,25 +1,33 @@
+import React from 'react';
+
 function CompanyDescription() {
-    return (
-      <div className="py-12 bg-gradient-to-r from-blue-300 via-blue-200 to-blue-100">
-        <div className="max-w-6xl mx-auto px-6">
-          <h2 className="text-4xl font-extrabold text-gray-900 mb-6 text-center lg:text-left">
+  // Tailwind CSS classes for the background image
+  const backgroundImageStyle = "bg-[url('/bg2.png')] bg-cover bg-no-repeat bg-center";
+
+  return (
+    <div className={`${backgroundImageStyle} min-h-screen flex items-center justify-center`}>
+      <div className="w-full max-w-5xl mx-auto p-10 md:py-16 bg-black bg-opacity-75 rounded-xl shadow-xl">
+        <div className="text-center space-y-6">
+          <h2 className="text-5xl font-bold text-indigo-400">
             Who We Are
           </h2>
-          <div className="bg-white rounded-lg shadow-xl overflow-hidden p-8">
-            <p className="text-lg text-gray-800 leading-relaxed mb-6">
-              WebOpMatics is at the cutting-edge intersection of web development, hosting, and IT consulting. Our mastery in crafting dynamic web experiences and strategic IT solutions empowers businesses to thrive in the digital era.
-            </p>
-            <p className="text-lg text-gray-800 leading-relaxed mb-6">
-              With a visionary approach to technology, our team delivers bespoke solutions that resonate with your brand's voice and mission. We excel in building responsive, high-performance websites and providing robust hosting services to ensure your online presence is second to none.
-            </p>
-            <p className="text-lg text-gray-800 leading-relaxed">
-              Embark on your digital transformation journey with WebOpMatics. Experience seamless integration of technology with unparalleled customer support, propelling your business towards unparalleled success.
-            </p>
-          </div>
+          <p className="text-xl font-medium text-gray-300">
+            WebOpMatics is at the cutting-edge intersection of web development, hosting, and IT consulting. Our mastery in crafting dynamic web experiences and strategic IT solutions empowers businesses to thrive in the digital era.
+          </p>
+          <div className="border-b border-gray-700 my-6" />
+          <p className="text-lg text-gray-400">
+            With a visionary approach to technology, our team delivers bespoke solutions that resonate with your brand's voice and mission. We excel in building responsive, high-performance websites and providing robust hosting services to ensure your online presence is second to none.
+          </p>
+          <p className="text-lg text-gray-400">
+            Embark on your digital transformation journey with WebOpMatics. Experience seamless integration of technology with unparalleled customer support, propelling your business towards unparalleled success.
+          </p>
+          <a href="/contact" className="mt-10 inline-block bg-indigo-600 text-white font-bold py-3 px-6 rounded-lg hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-opacity-50 transition duration-300">
+            Contact Us
+          </a>
         </div>
       </div>
-    );
-  }
-  
-  export default CompanyDescription;
-  
+    </div>
+  );
+}
+
+export default CompanyDescription;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
-// tailwind.config.js
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          100: '#e6f0ff',
+          200: '#ccdfff',
+          300: '#b3ceff',
+          400: '#99bdff',
+          500: '#80acff', // primary brand color
+          600: '#668bcc',
+          700: '#4d6a99',
+          800: '#334966',
+          900: '#1a2933',
+        },
+        // Add more color scales as needed
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+        // Add more custom fonts or other font categories as needed
+      },
+      boxShadow: {
+        'custom-light': '0 2px 4px rgba(0, 0, 0, 0.1)',
+        'custom-dark': '0 2px 4px rgba(0, 0, 0, 0.2)',
+        // Define other custom shadows if needed
+      },
+      // Add other theme extensions like spacing, fontSize, etc., as needed
+    },
   },
   plugins: [],
+  // Add any Tailwind CSS plugins you might need
 };
